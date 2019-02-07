66a60531a53ed5dbae91948cfddc457a8a18ccf9
Moududur "Moody" Rahman <mrahman01@stuy.edu>
2019-02-07 11:29:51 -0500

    initial private 2d array, main method

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,7 +1,22 @@
 public class QueenBoard{
 
-public QueenBoard(){
+private int[][] board;
 
+public QueenBoard(int size){
+	board = new int[size][size];
+	for(int x = 0; x < size;x++){
+		for (int y = 0; y < size; y++){
+			System.out.print(board[x][y]);
+			System.out.print(" ");
+		}
+		System.out.println();
+	}
 }
+
+
+public static void main(String[] args) {
+	QueenBoard q = new QueenBoard(8);
+}
+
 
 }
