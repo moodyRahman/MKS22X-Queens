de61ae23613d512ad7a3de949b96a17806abe19f
Moududur "Moody" Rahman <mrahman01@stuy.edu>
2019-02-07 11:35:04 -0500

    basic queen placer, no danger zone detection | added toString

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,6 +1,7 @@
 public class QueenBoard{
 
 private int[][] board;
+private int size;
 
 public QueenBoard(int size){
 	board = new int[size][size];
@@ -11,11 +12,30 @@ public QueenBoard(int size){
 		}
 		System.out.println();
 	}
+	this.size = size;
+}
+
+public boolean placeQueen(int x, int y){
+	board[x][y] = -1;
+	return true;
+}
+
+public String toString(){
+	String output = "";
+	for(int x = 0; x < size;x++){
+		for (int y = 0; y < size; y++){
+			output += board[x][y];
+			output += " ";
+		}
+		output += "\n";
+	}
+	return output;
 }
 
 
 public static void main(String[] args) {
 	QueenBoard q = new QueenBoard(8);
+	q.placeQueen(0, 0);
 }
 
 
